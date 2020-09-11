f74a8e197dbbcb9efaf396c939cff9fd9025a872
Administrator <2605030017@qq.com>
2020-09-11 18:02:52 +0800

    添加部分代码

--- common/src/main/java/com/app/common/MyApplication.java
+++ common/src/main/java/com/app/common/MyApplication.java
@@ -7,6 +7,10 @@ import com.app.common.utils.LogUtils;
 import com.app.common.utils.Utils;
 import com.orhanobut.hawk.Hawk;
 
+import org.jetbrains.annotations.NotNull;
+
+import timber.log.Timber;
+
 public class MyApplication extends Application {
 
     @Override
@@ -17,6 +21,19 @@ public class MyApplication extends Application {
         //Hawk
         Hawk.init(this).build();
         //Timer初始化
-        TimberUtils.setLogDebug();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            Timber.tag("App");
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, @NotNull String message, Throwable t) {
+            //线上环境所做的log处理
+            //null
+        }
     }
 }
